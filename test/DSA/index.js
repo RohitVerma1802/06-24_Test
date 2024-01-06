e3cc23289695fe4c4fdb5710785191dd484efaca
RohitV27 <vermar9867@gmail.com>
2024-01-06 15:47:43 +0530

    DSA completed

---
+++ test/DSA/index.js
@@ -0,0 +1,31 @@
+// nums[i]
+// nums[j]
+// val K
+// function (int nums, int val)
+// for (int i = 0; int < nums.length; i++) {
+//     if (nums[i] != temp) {
+// for (int j = 0; j < n + 1; j++) {
+//     nums[i] = temp;
+//     temp = nums[j];
+//     nums[j] = temp;
+// }
+//     }
+// }
+
+
+// const array = [1, 2, 3, 4, 5]
+
+// for(i = 0; i < array.length; i++) {
+//     if()
+
+// }
+// return;
+
+
+// int
+// if(i = 0; i < array.length; i++) {
+//     if(num[i] == +1)
+// }
+// return;
+
+
